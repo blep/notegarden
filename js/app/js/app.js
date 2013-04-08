4f74e81722a42eaf9a74dd46a7c2bbad48cc35be
Baptiste Lepilleur <baptiste.lepilleur@gmail.com>
2013-04-08 10:23:23 +0200

    started preparing for catalog page edition

--- js/app/js/app.js
+++ js/app/js/app.js
@@ -4,6 +4,9 @@
 // Declare app level module which depends on filters, and services
 angular.module('noteGarden', ['noteGarden.filters', 'noteGarden.services', 'noteGarden.directives']).
   config(['$routeProvider', function($routeProvider) {
+    $routeProvider.when('/catalog/:catalogId/edit', {
+        templateUrl: 'partials/catalog-edit.html',
+        controller: CatalogEditController } );
     $routeProvider.when('/notes', {templateUrl: 'partials/notes-list.html', controller: NoteListController});
     $routeProvider.when('/notes/:noteId', {templateUrl: 'partials/note-detail.html', controller: NoteDetailController});
     $routeProvider.otherwise({redirectTo: '/notes'});
